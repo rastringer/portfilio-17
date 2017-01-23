aee4ee5a309cd160b356fb4e1cb8ef69fadb4b5b
rastringer <ra1stringer@gmail.com>
2017-01-23 00:11:07 -0800

    adds server for deployment

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+
+var express = require('express');
+var app = express();
+
+// Run the app by serving the static files
+// in the dist directory
+
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default
+// Heroku port
+
+app.listen(process.env.PORT || 8080);
